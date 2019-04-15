c5a70b85e4920724a1670458ecfab1f4fa9b4afc
rabbit <ania.popescu@yahoo.com>
2019-04-15 16:35:51 +0300

    Add solution/tests

---
+++ ProblemTwo.cs
@@ -0,0 +1,105 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public class ProblemTwo
+    {
+        private const string Hash = "#";
+        private const string Space = " ";
+        private const string NewLine = "\n";
+
+        //Extracted this as a private field, to avoid passing it as an argument to all the methods.
+        //Initialized it in the constructor.
+        private readonly StringBuilder _result;
+
+        public ProblemTwo()
+        {
+            this._result = new StringBuilder();
+        }
+
+        public string DrawSquares(int[] edges)
+        {
+            //check if the edges array contains anything
+            if (edges == null || edges.Length == 0)
+            {
+                return string.Empty;
+            }
+
+            //get maximum edge value
+            //this is the last element in the array, since we know it is already sorted in ascending order
+            var maxWidth = edges[edges.Length - 1];
+
+            //draw first row
+            DrawHorizontalLine(maxWidth);
+
+            //draw middle rows (except first and last)
+            DrawSquareBody(edges, maxWidth);
+
+            //draw last row, if the maximum edge is greater that 1, otherwise there is no need
+            if (maxWidth > 1)
+            {
+                DrawHorizontalLine(maxWidth);
+            }
+
+            //return result by removing trailing white spaces
+            return _result.ToString().TrimEnd();
+        }
+
+        /// <summary>
+        /// Draws a horizontal line with # only
+        /// </summary>
+        /// <param name="length">The length of the line</param>
+        private void DrawHorizontalLine(int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                _result.Append(Hash);
+            }
+
+            _result.Append(NewLine);
+        }
+
+        /// <summary>
+        /// Draws body of the square, except first and last line
+        /// </summary>
+        /// <param name="edges">The array containing integers - sorted in ascending order</param>
+        /// <param name="width">The length of the square</param>
+        private void DrawSquareBody(int[] edges, int width)
+        {
+            for (int i = 2; i < width; i++)
+            {
+                for (int j = 1; j <= width; j++)
+                {
+                    if (IsFirstOrLastElement(j, width) || IsOnInnerSquareEdge(i, j, edges))
+                    {
+                        _result.Append(Hash);
+                    }
+                    else
+                    {
+                        _result.Append(Space);
+                    }
+                }
+
+                _result.Append(NewLine);
+            }
+        }
+
+        private static bool IsFirstOrLastElement(int position, int length)
+        {
+            return position == 1 || position == length;
+        }
+
+        /// <summary>
+        /// Checks if the given coordinates is on the edge of an inner square (more specifically, bottom edge and right edge)
+        /// </summary>
+        /// <param name="i">The current line</param>
+        /// <param name="j">The current row</param>
+        /// <param name="edges">The edges array</param>
+        /// <returns>True, if coordinates are on the edge, false otherwise</returns>
+        private static bool IsOnInnerSquareEdge(int i, int j, int[] edges)
+        {
+            return edges.Contains(i) && j <= i || edges.Contains(j) && i < j;
+        }
+    }
+}
---
+++ ProblemTwoTests.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleApp2
+{
+    public class ProblemTwoTests
+    {
+        [Test]
+        public void ProblemTwo_EdgesIsNull_ReturnEmptyString()
+        {
+            ProblemTwo problem = new ProblemTwo();
+
+            string result = problem.DrawSquares(null);
+
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [Test]
+        public void ProblemTwo_EdgesIsEmpty_ReturnEmptyString()
+        {
+            ProblemTwo problem = new ProblemTwo();
+
+            string result = problem.DrawSquares(new int[] { });
+
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [Test]
+        public void ProblemTwo_EdgesHasOneElementOfValue1_Returns1Hash()
+        {
+            ProblemTwo problem = new ProblemTwo();
+
+            string result = problem.DrawSquares(new[] { 1 });
+
+            Assert.AreEqual("#", result);
+        }
+
+        [Test]
+        public void ProblemTwo_EdgesHasOneElementOfValue2_ReturnsExpectedSquare()
+        {
+            ProblemTwo problem = new ProblemTwo();
+
+            string result = problem.DrawSquares(new[] { 2 });
+
+            Assert.AreEqual("##\n##", result);
+        }
+
+        [Test]
+        public void ProblemTwo_EdgesHasElements1and2_ReturnsExpectedSquare()
+        {
+            ProblemTwo problem = new ProblemTwo();
+
+            string result = problem.DrawSquares(new[] { 1, 2 });
+
+            Assert.AreEqual("##\n##", result);
+        }
+
+        [Test]
+        public void ProblemTwo_EdgesHasTwoElements_ReturnsExpectedSquare()
+        {
+            ProblemTwo problem = new ProblemTwo();
+
+            string result = problem.DrawSquares(new[] { 3, 5 });
+
+            Assert.AreEqual("#####\n# # #\n### #\n#   #\n#####", result);
+        }
+
+        [Test]
+        public void ProblemTwo_EdgesHasMultipleRandomElements_ReturnsExpectedSquare()
+        {
+            ProblemTwo problem = new ProblemTwo();
+
+            string result = problem.DrawSquares(new[] { 1, 3, 5, 6, 11 });
+
+            string expectedResult = "###########\n" +
+                                    "# # ##    #\n" +
+                                    "### ##    #\n" +
+                                    "#   ##    #\n" +
+                                    "######    #\n" +
+                                    "######    #\n" +
+                                    "#         #\n" +
+                                    "#         #\n" +
+                                    "#         #\n" +
+                                    "#         #\n" +
+                                    "###########";
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
